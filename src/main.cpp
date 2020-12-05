4e4554349b57ec3cb0388849b120ed1bee3de37d
vojtavojta <vojtahranicka@seznam.cz>
2020-12-05 14:14:46 +0100

    error in main

--- src/main.cpp
+++ src/main.cpp
@@ -11,109 +11,111 @@
 
 
 //auto narocne = std::make_shared<Facility>("Narocne");
-auto normalni = std::make_shared<Facility>("Normalni");
-auto kosik = std::make_shared<Resources>("kosik", 4);
+auto cashDesk = std::make_shared<Facility>("Cash_desk");
+auto shoppingTrolley = std::make_shared<Resources>("shopping_trolley", 4);
+auto delicatessen = std::make_shared<Resources>("delicatessen", 2);
 
 
 
-class working: public RREvent {
+class leaving: public RREvent {
     void behaviour() override {
         for (int i = 0; i < this->resource_handler.size(); i++) {
             this->resource_handler[i]->release();
+           // if (this->resource_handler[i]->service_line->name == "shopping_trolley") {
+             //       this->resource_handler[i]->release();
+                //}
         }
-        std::cout << "zakaznik release " << this->get_id() << "\n";
+
+
     }
 };
 
-class work: public RREvent {
+class payment: public RREvent {
     void behaviour() override {
-        std::cout << "zakaznik work " << this->get_id() << "\n";
-        auto a = normalni->seize_or_reserve();
+        std::cout << "customer payment " << this->get_id() << "\n" ;
+        auto l = std::make_shared<leaving>();
+        l->resource_handler = this->resource_handler;
+        l->plan(current_time + 20); //RAND
+    };
+};
+
+class goods_selected: public RREvent {
+    void behaviour() override {
+        std::cout << "goods selected" << this->get_id() << "\n";
+        auto a = cashDesk->seize_or_reserve();
         auto d = this->resource_handler;
         auto l = [d](){
-            std::cout << "kosik leave\n";
-            for (int i = 0; i < d.size(); i++) {
-                if (d[i]->service_line->name == "kosik") {
+            std::cout << "ERROR customer cant leave without paying\n";
+            /*for (int i = 0; i < d.size(); i++) {
+                if (d[i]->service_line->name == "shoppingTrolley") {
                     d[i]->release();
                 }
-            }
+            }*/
         };
-        a->on_fail(20, l);
-        auto workingg = std::make_shared<working>();
-        workingg->resource_handler = this->resource_handler;
-        a->on_success(workingg);
+        a->on_fail(120000, l);
+        auto p = std::make_shared<leaving>();
+        p->resource_handler = this->resource_handler;
+        a->on_success(p);
         
     }
 };
 
-class working_weak:public RREvent{
-    void behaviour() override {
-        this->resource_handler[0]->release();
-        std::cout << "zakaznik weak release " << this->get_id() << "\n";
-    }
-};
 
-class weak_work: public RREvent {
+class shoping: public RREvent {
     void behaviour() override {
-        std::cout << "zakaznik weak work " << this->get_id() << "\n" ;
-        auto workingg = std::make_shared<working_weak>();
-        workingg->resource_handler = this->resource_handler;
-        workingg->plan(current_time + 10);
+        std::cout << "customer is shoping" << this->get_id() << "\n" ;
+        auto goods = std::make_shared<goods_selected>();
+        goods->resource_handler = this->resource_handler;
+        goods->plan(current_time + 15);//RAND
     };
 };
 
-class leave: public Event{
-    void behaviour() override{
-        std::cout << "weak work leaving " << this->get_id() << "\n";
-    }
-};
 
-class zakaznik:public Event{
+class choosing_customer_type: public RREvent {
     void behaviour() override {
-        if( Uniform_Random(0, 100) < 33 ){
-            std::cout << "zakaznik created " << this->get_id() << "\n";
-            auto a = kosik->seize_or_reserve(1);
-            auto w = std::make_shared<work>();
-            auto l = [](){
-                std::cout << "I am leaving\n";
-            };
-            a->on_fail(20, l);
-            a->on_success(w);
-        } else {
-            std::cout << "zakaznik weak created " << this->get_id() << "\n";
-            auto a = normalni->seize_or_reserve();
-//            auto w = std::make_shared<weak_work>();
-            auto r = a->resource_handler;
-            auto w = [](std::vector<std::shared_ptr<ResourceHandler>> r){
-                std::cout << "zakaznik weak work in LAMBDA \n";
-                auto workingg = std::make_shared<working_weak>();
-                workingg->resource_handler = r;
-                workingg->plan(current_time + 10);
-            };
-            auto l = std::make_shared<leave>();
-            a->on_fail(10, l);
-            a->on_success(w);
+        if(Norm_Random(0, 100) > 70){
+            auto s = std::make_shared<shoping>();
+            s->resource_handler = this->resource_handler;
+            s->plan();
+        }
+        else {
+            auto m = std::make_shared<shoping>();
+            m->resource_handler = this->resource_handler;
+            m->plan();
         }
     }
 };
 
-class generator: public Event{
+class take_trolley:public Event{
+    void behaviour() override {
+            std::cout << "customer created " << this->get_id() << "\n";
+            auto a = shoppingTrolley->seize_or_reserve(1);
+            auto w = std::make_shared<choosing_customer_type>();
+            auto l = [](){
+                std::cout << "I am leaving\n";
+            };
+            a->on_fail(1, l);
+            a->on_success(w);
+    }
+};
+
+class customer_generator: public Event{
     void behaviour() override{
-        auto a = std::make_shared<zakaznik>();
+        auto a = std::make_shared<take_trolley>();
         a->plan();
-        this->plan(current_time + (rand()%20));
+        this->plan(current_time + (rand()%20)); //RAND
     }
 };
 
 int main(int argc, const char * argv[]) {
     std::cout << "start\n";
-    auto sim = std::make_shared<Simulator>(0, 200);
+    auto sim = std::make_shared<Simulator>(0, 5000);
     std::cout << "simulator created\n";
-    auto a = std::make_shared<generator>();
+    auto a = std::make_shared<customer_generator>();
     a->plan();
-    std::cout << "genterator prepared\n";
+    std::cout << "customer_genterator prepared\n";
     sim->run();
     simulation_info->print_out();
-    generated_numbers_statistics->print_out();
+    //generated_numbers_statistics->print_out();
     return 0;
 }
